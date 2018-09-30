c48c0490e81684a85015ef29c44bbbabd7201e9d
Christina-Ilyushonok <iluyshonok.kristina@gmail.com>
2018-09-30 15:21:54 +0300

    16/18

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsSet = new Set();
+    bracketsSet.add("(").add(")");
+    bracketsSet.add("[").add("]");
+    bracketsSet.add("{").add("}");
+    bracketsSet.add("|");
+    let prevStrLength = 0;
+    while (prevStrLength !== str.length && str.length > 0) {
+        prevStrLength = str.length;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let trueBrackets = '';
+            let brackets = bracketsConfig[i];
+            if (bracketsSet.has(brackets[0])) {
+                trueBrackets +=  brackets[0];
+            }
+            if (bracketsSet.has(brackets[1])) {
+                trueBrackets += brackets[1];
+            }
+            str = str.replace(trueBrackets, '');
+        }
+    }
+    return str.length === 0;
 }
